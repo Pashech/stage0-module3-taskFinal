75038e0178ea270bd3238d63658a0cd097900da9
Pavel Setsko <Pashech555@gmail.com>
2022-07-15 14:02:15 +0300

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int a, b, c, d;
+        int sum;
+        a = number % 10;
+        b = number / 10 % 10;
+        c = number / 10 /10 % 10;
+        d = number / 10 / 10 / 10 % 10;
+        sum = a + b + c +d;
+
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int a, b, c;
+        a = number % 10;
+        b = number / 10 % 10;
+        c = number / 10 / 10 % 10;
+        System.out.println(a + "" + b + "" + c);
     }
 }
